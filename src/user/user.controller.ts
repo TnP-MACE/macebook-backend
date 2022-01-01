658682e87b824732d9edff97ebf78abc78cb349a
Sruthy M L <sruthyml328@gmail.com>
2022-01-01 15:26:05 +0530

    user module swagger updation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,7 +7,9 @@ import localAuthenticationGuard from './guards/local-auth.guard'
 import { Response } from 'express';
 import RequestWithUser from './interfaces/requestWithUser.interface';
 import { ChangePasswordDto } from './dto/change-password.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { EditUsernameDto } from './dto/edit-username.dto';
+import { DeleteAccountDto } from './dto/delete-account.dto';
 
 @ApiTags('Authentication & User')
 @Controller('api/v1/auth')
@@ -28,32 +30,10 @@ export class UserController {
     return {...request.user, access_token: accessToken }
   }
 
-
-  @ApiBearerAuth()
-  @UseGuards(jwtAuthenticationGuard)
-  @Post('logout')
-  async logOut(@Req() request, @Res() response: Response) {
-    response.setHeader('Set-Cookie', this.userService.getCookieForLogOut());
-    return response.sendStatus(200);
-  }
-
-  @ApiBearerAuth()
-  @UseGuards(jwtAuthenticationGuard)
-  @Delete('delete-account')
-  async deleteUser(@Body() body, @Req() req : RequestWithUser){
-    try{
-      const {password} = body;
-      const {uid} = req.user;
-      return await this.userService.deleteUser(uid,password); 
-    } catch (err){
-      throw err;
-    }
-  }
-
   @ApiBearerAuth()
   @UseGuards(jwtAuthenticationGuard)
   @Patch('edit-username')
-  async editUsername(@Body() body, @Req() req: RequestWithUser){
+  async editUsername(@Body() body : EditUsernameDto, @Req() req: RequestWithUser){
     const {uid} = req.user;
     const {username} = body;
     return await this.userService.editUsername(uid,username)
@@ -73,11 +53,34 @@ export class UserController {
 
   @ApiBearerAuth()
   @UseGuards(jwtAuthenticationGuard)
+  @Delete('delete-account')
+  async deleteUser(@Body() body: DeleteAccountDto, @Req() req : RequestWithUser){
+    try{
+      const {password} = body;
+      const {uid} = req.user;
+      return await this.userService.deleteUser(uid,password); 
+    } catch (err){
+      throw err;
+    }
+  }
+
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get details of logged in user' })
+  @UseGuards(jwtAuthenticationGuard)
   @Get()
   authenticate(@Req() request: RequestWithUser) {
     const user = request.user;
     user.password = undefined;
     return user;
   }
+
+  // Log Out Feature ( not to be used )
+  // @ApiBearerAuth()
+  // @UseGuards(jwtAuthenticationGuard)
+  // @Post('logout')
+  // async logOut(@Req() request, @Res() response: Response) {
+  //   response.setHeader('Set-Cookie', this.userService.getCookieForLogOut());
+  //   return response.sendStatus(200);
+  // }
   
 }
